f03debd785a56d583a09d27a8d26e96002a7e976
Anja Fullgrabe <anjaf@ebi.ac.uk>
2018-10-17 14:17:45 +0100

    a s cript that checks for terms being mapped to different ontology terms

---
+++ tests/validate_mappings_file.py
@@ -0,0 +1,27 @@
+import codecs
+from collections import defaultdict
+import sys
+
+
+def check_for_mapping_conflict():
+    """This test checks if a single term has been mapped two more than one
+    ontology terms"""
+    with codecs.open("../zoomage_report.CURATED.tsv", encoding='utf-8') as f:
+        term_dict = defaultdict(set)
+        header = f.readline()
+        for line in f:
+            values = [value.strip() for value in line.split('\t')]
+            category, term, ontology_ref, study, assay, annotator, date = values
+            term_dict[term].add(ontology_ref)
+
+        conflicts = [(t, url) for t, url in term_dict.items() if len(term_dict[t]) > 1]
+        if conflicts:
+            for mapping in conflicts:
+                print("{} has been mapped to multiple terms:\t{}".format(mapping[0],
+                 ', '.join(mapping[1])))
+
+            sys.exit(1)
+
+
+if __name__ == '__main__':
+    check_for_mapping_conflict()
