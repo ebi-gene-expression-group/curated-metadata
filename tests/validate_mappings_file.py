6bdc86c2f8237fdc130074e641752a85575e1a7b
Pedro Madrigal <8195212+pmb59@users.noreply.github.com>
2022-10-10 10:55:31 +0100

    Update validate_mappings_file.py

--- tests/validate_mappings_file.py
+++ tests/validate_mappings_file.py
@@ -3,6 +3,7 @@ from collections import defaultdict
 import sys
 
 
+
 def check_for_mapping_conflict():
     """This test checks if a single term has been mapped two more than one
     ontology terms"""
